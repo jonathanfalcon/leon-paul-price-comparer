6ad92a2e99656248c3d86a1baf9e3f9b66fac935
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-01 14:02:20 -0500

    Exported all web scraping types from main index file

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,3 +1,18 @@
 /**
  * Exports all the types used in the Leon Paul Price Comparer project.
  */
+
+// Web Scraping Types
+export * from './LeonPaulPageData/LeonPaulPageData'
+
+// Web Scraping Types - MagentoInit
+export * from './LeonPaulPageData/MagentoInit/MagentoInit'
+export * from './LeonPaulPageData/MagentoInit/SimpleProduct'
+export * from './LeonPaulPageData/MagentoInit/ConfigurableProduct'
+export * from './LeonPaulPageData/MagentoInit/BundleProduct'
+
+// Web Scraping Types - Product
+export * from './LeonPaulPageData/Product/Product'
+
+// Web Scraping Types - Common
+export * from './LeonPaulPageData/Common/Prices'
