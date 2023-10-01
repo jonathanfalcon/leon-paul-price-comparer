94db080a82dc5aad4ac2987117da97325cf273eb
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-01 13:24:27 -0500

    Added a utility function to extract url components

--- shared/validation/leonPaulUrl.ts
+++ shared/validation/leonPaulUrl.ts
@@ -19,3 +19,55 @@ type UrlComponents = {
     query: string,
     anchor: string,
 }
+
+
+/**
+ * Dissects a given URL into its main components. The URL is standardized it in the process by forcing protocol to `https` and suffix to `html`.
+ * @param urlString A URL in string format.
+ * @returns Returns an object with the protocol, subdomain, domain, path, extension, query params, and anchor tags. An absent component is represented by an empty string. An invalid url will return an object where all components are an empty string.
+ *
+ * @example
+ * const url = 'http://www.leonpaul.com/project-zero-foil?utm_medium=email#specs'
+ * console.log(extractUrlComponents(url))
+ *
+ * // This will be logged to the console:
+ * {
+ *    protocol: 'https',
+ *    subdomain: 'www',
+ *    domain: 'leonpaul.com',
+ *    path: '/project-zero-foil.html',
+ *    extension: 'html',
+ *    query: 'utm_medium=email',
+ *    anchor: '#specs'
+ * }
+ */
+const extractUrlComponents = (urlString: string): UrlComponents => {
+    try {
+        const urlDirty = new URI(urlString)
+            .protocol('https')
+            .suffix('html')
+
+        const url = new URI(urlDirty.readable())
+        url.normalize()
+
+        return {
+            protocol: url.protocol() || '',
+            subdomain: url.subdomain() || '',
+            domain: url.domain() || '',
+            path: url.path() || '',
+            extension: url.suffix() || '',
+            query: url.query() || '',
+            anchor: url.hash() || '',
+        }
+    } catch(error) {
+        return {
+            protocol: '',
+            subdomain: '',
+            domain: '',
+            path: '',
+            extension: '',
+            query: '',
+            anchor: '',
+        }
+    }
+}
