a0120c7dcda369312d4a93a3e5c3fc72ad9d5352
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-01 14:01:06 -0500

    Created type LeonPaulPageData to represent the data collected from scraping a Leon Paul product page

---
+++ shared/types/LeonPaulPageData/LeonPaulPageData.ts
@@ -0,0 +1,17 @@
+import {Product} from './Product/Product'
+import {MagentoInit} from './MagentoInit/MagentoInit'
+
+
+/**
+ * Represents the data collected from scraping a Leon Paul product page.
+ *
+ * @property product - Contains all basic product information unrelated to pricing.
+ * @property magentoInit - Contains all pricing information, as well as product attributes and options.
+ *
+ * @see Product
+ * @see MagentoInit
+ */
+export type LeonPaulPageData = {
+    product: Product,
+    magentoInit: MagentoInit,
+}
