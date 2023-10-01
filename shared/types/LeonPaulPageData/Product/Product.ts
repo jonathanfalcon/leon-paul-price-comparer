1a68afb4c99f9ae271f8dd8eb81f195d100f60c4
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-01 13:55:33 -0500

    Created type Product to represent data in window.dataLayer

---
+++ shared/types/LeonPaulPageData/Product/Product.ts
@@ -0,0 +1,16 @@
+/**
+ * Represents the structure of product object in `window.dataLayer`.
+ *
+ * @property id - The unique identifier for a given product.
+ * @property sku - The SKU of a given product. This will differ from `id`, and is valid for search on Leon Paul's website, unlike `id`.
+ * @property name - The name for a given product.
+ * @property product_type - The product type of a given product. Can only be one of three values: `simple`, `configurable`, or `bundle`.
+ * @property image_url - The url for a given product's image.
+ */
+export type Product = {
+    id: string,
+    sku: string,
+    name: string,
+    product_type: 'simple' | 'configurable' | 'bundle',
+    image_url: string,
+}
