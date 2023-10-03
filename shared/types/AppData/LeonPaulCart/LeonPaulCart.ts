64d6cacdec6bcefc09a267a6e0a0dc9ac1413288
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-02 21:19:29 -0500

    Added generic types to LeonPaulRegionalProductCollection LeonPaulCartItemBase
    Created three cart items reflecting available product types
    
    Modified LeonPaulCartItem to ensure all regional product data is of the same product type

--- shared/types/AppData/LeonPaulCart/LeonPaulCart.ts
+++ shared/types/AppData/LeonPaulCart/LeonPaulCart.ts
@@ -1,5 +1,5 @@
-import {CountryCode} from '../..'
-import {LeonPaulProduct} from './LeonPaulProduct'
+import {CountryCode, ProductType} from '../..'
+import {LeonPaulProduct, LeonPaulSimpleProduct, LeonPaulConfigurableProduct, LeonPaulBundleProduct} from './LeonPaulProduct'
 
 /**
  * Represents all the regional product data.
@@ -7,28 +7,65 @@ import {LeonPaulProduct} from './LeonPaulProduct'
  * @see CountryCode
  * @see LeonPaulProduct
  */
-export type LeonPaulRegionalProductCollection = {
-    [country in CountryCode]: LeonPaulProduct
+export type LeonPaulRegionalProductCollection<Product extends LeonPaulProduct> = {
+    [country in CountryCode]: Product
 }
 
 
 /**
- * Represents an item found in the web app's Leon Paul cart. Contains general product information and regional product information.
+ * Represents the common structure of an item found in the web app's Leon Paul cart. Contains general product information and regional product information.
  * @property sku - The SKU of a given product. This is valid for search on Leon Paul's website.
  * @property imageUrl - The url for a given product's image.
  * @property quantity - The quantity of this item in the cart.
  * @property lastFetch - A timestamp indicating when the product data was last fetched and updated. It is generated and stored in Coordinated Universal Time (UTC).
  * @property index - Indicates the position of this item in the cart. Used for repositioning and keeping track of order.
- *
- * @see LeonPaulRegionalProductCollection
+ * @property productType - The product type of a given product. Can only be one of three values: `simple`, `configurable`, or `bundle`.
  */
-export type LeonPaulCartItem = {
+export type LeonPaulCartItemBase<ProductTypeString extends ProductType> = {
     sku: string,
     imageUrl: string,
     quantity: number,
     lastFetch: number,
     index: number,
-} & LeonPaulRegionalProductCollection
+    productType: ProductTypeString,
+}
+
+
+/**
+ * Represents a simple item found in the web app's Leon Paul cart, where `productType: 'simple'`.
+ *
+ * @see LeonPaulCartItemBase
+ * @see LeonPaulRegionalProductCollection
+ */
+export type LeonPaulCartItemSimple = LeonPaulCartItemBase<'simple'> &  LeonPaulRegionalProductCollection<LeonPaulSimpleProduct>
+
+
+/**
+ * Represents a configurable item found in the web app's Leon Paul cart, where `productType: 'configurable'`.
+ *
+ * @see LeonPaulCartItemBase
+ * @see LeonPaulRegionalProductCollection
+ */
+export type LeonPaulCartItemConfigurable = LeonPaulCartItemBase<'configurable'> &  LeonPaulRegionalProductCollection<LeonPaulConfigurableProduct>
+
+
+/**
+ * Represents a bundle item found in the web app's Leon Paul cart, where `productType: 'bundle'`.
+ *
+ * @see LeonPaulCartItemBase
+ * @see LeonPaulRegionalProductCollection
+ */
+export type LeonPaulCartItemBundle = LeonPaulCartItemBase<'bundle'> &  LeonPaulRegionalProductCollection<LeonPaulBundleProduct>
+
+
+/**
+ * Represents all the possible cart items.
+ *
+ * @see LeonPaulCartItemSimple
+ * @see LeonPaulCartItemConfigurable
+ * @see LeonPaulCartItemBundle
+ */
+export type LeonPaulCartItem = LeonPaulCartItemSimple | LeonPaulCartItemConfigurable | LeonPaulCartItemBundle
 
 
 /**
