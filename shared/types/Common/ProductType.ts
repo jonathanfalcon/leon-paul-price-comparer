90a8f2adf0d80c0afd6e600ab801b1924ec4b8c3
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-02 20:37:34 -0500

    Added new type ProductType, ensuring consistency between app types and page data types

---
+++ shared/types/Common/ProductType.ts
@@ -0,0 +1,4 @@
+/**
+ * The product type of a given product. Can only be one of three values: `simple`, `configurable`, or `bundle`.
+ */
+export type ProductType = 'simple' | 'configurable' | 'bundle'
