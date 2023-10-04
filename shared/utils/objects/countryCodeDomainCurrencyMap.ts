e54e092c59fddaa9fa5ccdd1360d419e4e27dd2f
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-04 15:41:00 -0500

    Created and exported countryCodeDomainCurrencyMap object based on type CountryCodeDomainCurrencyMap

---
+++ shared/utils/objects/countryCodeDomainCurrencyMap.ts
@@ -0,0 +1,36 @@
+import { CountryCodeDomainCurrencyMap } from '@leon-paul-price-comparer/types'
+
+export const countryCodeDomainCurrencyMap: CountryCodeDomainCurrencyMap = {
+    AUS: {
+        domain: 'leonpaulaustralia.com',
+        currency: 'AUD',
+    },
+    CAN: {
+        domain: 'leonpaulcanada.com',
+        currency: 'CAD',
+    },
+    DEU: {
+        domain: 'leonpaulgermany.com',
+        currency: 'EUR',
+    },
+    FRA: {
+        domain: 'leonpaulfrance.com',
+        currency: 'EUR',
+    },
+    ITA: {
+        domain: 'leonpaulitaly.com',
+        currency: 'EUR',
+    },
+    GBR: {
+        domain: 'leonpaul.com',
+        currency: 'GBP',
+    },
+    UKR: {
+        domain: 'leonpaulukraine.com',
+        currency: 'UAH',
+    },
+    USA: {
+        domain: 'leonpaulusa.com',
+        currency: 'USD',
+    },
+}
