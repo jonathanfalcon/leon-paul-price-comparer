efaf5f884cee682072acd9ee6811160ebd66f380
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-05 13:19:04 -0500

    Added index.ts with exports inside of types/LeonPaulPageData

---
+++ shared/types/LeonPaulPageData/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Exports all Leon Paul Page Data types used in the Leon Paul Price Comparer project.
+ */
+
+// Common
+export * from './Common/Prices'
+
+// Magento Init
+export * from './MagentoInit/SimpleProduct'
+export * from './MagentoInit/ConfigurableProduct'
+export * from './MagentoInit/BundleProduct'
+export * from './MagentoInit/MagentoInit'
+
+// Product
+export * from './Product/Product'
+
+// Leon Paul Page Data
+export * from './LeonPaulPageData'
