2d05d8b18584387db123eb9e0351df3a7b6eb9fd
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-05 13:18:48 -0500

    Added index.ts with exports inside of types/AppData

---
+++ shared/types/AppData/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Exports all App Data types used in the Leon Paul Price Comparer project.
+ */
+
+// Exchange Rate
+export * from './ExchangeRate/ExchangeRate'
+
+// Leon Paul Cart
+export * from './LeonPaulCart/LeonPaulCart'
+export * from './LeonPaulCart/LeonPaulProduct'
+
+// User Preferences
+export * from './UserPreferences/UserPreferences'
