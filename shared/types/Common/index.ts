6a63d86682fd5fdaf0438a7c588a28dfa6b4ed37
Jonathan Falcon <JQFalcon@outlook.com>
2023-10-05 13:18:54 -0500

    Added index.ts with exports inside of types/Common

---
+++ shared/types/Common/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Exports all Common types used in the Leon Paul Price Comparer project.
+ */
+
+// Product Type
+export * from './ProductType'
+
+// Region Codes
+export * from './RegionCodes'
