f2990ecf01657fd24fab40f9240a5986b8cfb971
Jonathan Falcon <JQFalcon@outlook.com>
2023-12-07 13:43:15 -0600

    Added fetchIndividualExchangeRate function to allow for handling errors within individual exchange rate fetches and for better typing

--- backend/utils/data/fetchExchangeRate/fetchExchangeRate.ts
+++ backend/utils/data/fetchExchangeRate/fetchExchangeRate.ts
@@ -1,8 +1,52 @@
-import { ExchangeRateMap } from '@leon-paul-price-comparer/types'
+import { CurrencyApiResponse, CurrencyCode, ExchangeRateMap } from '@leon-paul-price-comparer/types'
+import { ExchangeRateError } from '../../exceptions'
 
 const { CURRENCY_API_KEY } = process.env
 
 const fetchExchangeRate = async () => {
+/**
+ * Fetches the exchange rate object from the currency api.
+ *
+ * @param url The url to fetch the exchange rate from.
+ * @param baseCurrency The base currency to fetch the exchange rate for.
+ *
+ * @returns A promise that resolves to a `CurrencyApiResponse` object.
+ *
+ * @throws {ExchangeRateError} If the response status indicates an HTTP error.
+ */
+const fetchIndividualExchangeRate = async <Currency extends CurrencyCode[]>(
+    url: string,
+    baseCurrency: CurrencyCode,
+): Promise<CurrencyApiResponse<Currency>> => {
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    setTimeout(() => {
+        controller.abort()
+    }, 8000)
+
+    try {
+        const response = await fetch(url, { signal })
+
+        if (response.ok) {
+            return await response.json()
+        } else {
+            throw new ExchangeRateError('HttpError', {
+                currency: baseCurrency,
+                status: response.status,
+            })
+        }
+    } catch (error) {
+        switch (error.name) {
+            case 'NetworkError':
+                throw new ExchangeRateError('NetworkError', { currency: baseCurrency })
+            case 'AbortError':
+                throw new ExchangeRateError('TimeoutError', { currency: baseCurrency })
+            default:
+                throw error
+        }
+    }
+}
     const baseUrl = 'https://api.currencyapi.com/v3/latest?apikey='
 
     const currencyPairs = {
