875757f0b23a7290ecec3e75ce06dd80d25c6e6c
Jonathan Falcon <JQFalcon@outlook.com>
2023-12-28 00:02:14 -0600

    Added connection to database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { exchangeRateRoute } from './routes/exchangeRate'
+import { database } from '../utils/database'
 
 const app = new Hono()
+
+database.connect()
 
 exchangeRateRoute(app)
 
--- backend/utils/database/index.ts
+++ backend/utils/database/index.ts
@@ -1,9 +1,11 @@
 import { connect } from 'mongoose'
 import { exchangeRate } from './access/exchangeRate'
 
-const connectDb = async (databaseURL: string) => {
+const { DATABASE_URL } = process.env
+
+const connectDb = async (): Promise<void> => {
     try {
-        await connect(databaseURL)
+        await connect(DATABASE_URL as string)
         console.log('Successfully connected!')
     } catch (error) {
         console.error('Unsuccessfully Connection:', error)
